192236d38bf6f2dab677716d8b20660f5ea3afd1
Ripunjay Sharma <erress@Ripunjays-MacBook-Pro.local>
2022-03-05 20:14:57 -0800

    Changes to flask api

--- trobo_api.py
+++ trobo_api.py
@@ -1,8 +1,7 @@
 import time, flask, pickle, json, sys
-#import pandas as pd
+import pandas as pd
 import numpy as np
 from googleapiclient import discovery
-
 
 # initialize flask object
 app = flask.Flask(__name__)
@@ -10,19 +9,8 @@ app = flask.Flask(__name__)
 #initialize API key for perpspective API
 API_KEY = 'AIzaSyC4zu-biGJBTDft7HBOFzzqQrrTMa1-JIs'
 
-# global dicts to store objects of TfidfVectorizer, MultiLabelBinarizer & trained model for each segment
-vectorizer = {}
-mlb = {}
-model = {}
-
-# global dict to store labels for classes in each segment
-class_label_mapping = {}
-
-# global list to store segments
-segments = ['bsd', 'dhs', 'gen']
-
 # compute model predictions using Perpective API
-def compute_predictions(test_term):
+def compute_predictions(test_term, strategy):
     # configuring the client for the Perpspective API
     client = discovery.build(
     'commentanalyzer',
@@ -35,40 +23,35 @@ def compute_predictions(test_term):
     # configuring the request
     analyze_request = {
     'comment': { 'text': test_term },
-    'requestedAttributes': {'FLIRTATION': {}, 'TOXICITY': {}, 'SEXUALLY_EXPLICIT': {}}
+    'requestedAttributes': {'SEVERE_TOXICITY': {}}
     }
    
     try:
         # retrun the rersponse from the Perspective API
         response = client.comments().analyze(body=analyze_request).execute()
 
-        # declare the response labels
-        labels = ['FLIRTATION', 'TOXICITY', 'SEXUALLY_EXPLICIT']
+        print('check')
 
         # categorize the response labels
-        flirtation = response['attributeScores']['FLIRTATION']['spanScores'][0]['score']['value']
-        toxicity = response['attributeScores']['TOXICITY']['spanScores'][0]['score']['value']
-        sexually_explicit = response['attributeScores']['SEXUALLY_EXPLICIT']['spanScores'][0]['score']['value']
+        sev_toxicity = response['attributeScores']['SEVERE_TOXICITY']['spanScores'][0]['score']['value']
 
-        return labels[np.argmax(np.array([flirtation, toxicity, sexually_explicit]))]
+
+        if strategy == "Toxic" and sev_toxicity > 0.5 :
+            return "TOXIC TOXIC TOXIC"
+        elif strategy == "Toxic":
+            return "NOT_TOXIC"
+        else:
+            df = pd.read_csv('/Users/erress/Desktop/Personal/GRE/IDP/Shared/Univ_California_SanDiego/WI22/Anti Social Computing/TroBo/Trobo/Trobo_data.csv')
+            print('check2')
+            print(strategy)
+            indices = df.index[df['STRATEGY'] == strategy]
+            print(indices)
+            return df.iloc[np.random.choice(indices)].DATA
+
 
     except Exception as exception:
         # return an invalid response
-        return -1
-
-# load class label mapping for each troll category
-def load_class_label_mapping(prediction):
-    # class_label_mapping used as global variable
-    global class_label_mapping
-    
-    try:
-        # load class label mapping from cached excel & convert to dict
-        class_label_mapping_df = pd.read_excel('Class_Label_Mapping.xlsx', index_col = 0)
-        class_label_mapping = class_label_mapping_df.to_dict('index')
-
-    except Exception as exception:
-        print('\n%s' % exception)
-        sys.exit(0)
+        return "No Prediction from Perspective API"
 
 @app.before_request
 def before_request():
@@ -80,7 +63,7 @@ def before_request():
 # serve predictions
 def predict():
    # dict served on api call
-    data = {'meme': [], 'joke': [], 'quote': [], 'success': False}
+    data = {'prediction': [], 'success': False}
 
     # get request parameters
     params = flask.request.json
@@ -93,41 +76,19 @@ def predict():
         test_term = params.get('query')
 
         # initialize strategy from params
-        strategy = params.get('strategy').lower()
+        strategy = params.get('strategy')
 
         # compute model predictions using Perpective API
-        prediction = compute_predictions(test_term)
-
-        # load class label mapping for each troll strategy
-        #load_class_label_mapping(prediction)
-    
-        # transform test term into Tfidf vector
-        #X_pred = vectorizer[segment].transform([term_test])
-        
-        # predict probabilities for test term
-        #prediction_probs = model[segment].predict(X_pred)[0]
-        
-        # store index of top n probabilities
-        #sorted_prediction_indexes = np.argsort(prediction_probs)[::-1][:5]
-    
-        # store top n predictions (i.e. classes) based on a threshold
-        #threshold = 0.3
-        #predictions = [mlb[segment].classes_[index] for index in sorted_prediction_indexes if prediction_probs[index] >= threshold]
+        pred = compute_predictions(test_term, strategy)
 
         time_1 = flask.g.request_time()
         
-        # map classes in predictions to labels
-        #labels = [class_label_mapping[segment][prediction] for prediction in predictions]
 
         # append predictions & labels to data
-        #data['predictions'].extend(predictions)
-        #data['labels'].extend(labels)
-        data['prediction'] = prediction
+        data['prediction'] = pred
         data['success'] = True
 
     time_2 = flask.g.request_time()
-    #data['Time_taken_by_class_label_mapping'] = time_2 - time_1
-    #data['Time_taken_without_class_label_mapping'] = time_1 
     data['Total_time_taken'] = time_2
         
     # return response in json format 
